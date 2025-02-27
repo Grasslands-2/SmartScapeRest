b2579b7a11b29aa981b09e76eae6b716584e8463
Matthew Bayles <mmbayles@gmail.com>
2025-02-27 01:03:20 -0600

    Changed from local to mnt storage

--- smartscape_rest/settings.py
+++ smartscape_rest/settings.py
@@ -19,8 +19,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 print(BASE_DIR)
-# SCRATCH_DIR = os.path.join("/", "mnt", "efs")
-SCRATCH_DIR = r"B:\Work\Grasslands\ss_rest_temp"
+SCRATCH_DIR = os.path.join("/", "mnt", "efs")
+# SCRATCH_DIR = r"B:\Work\Grasslands\ss_rest_temp"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
